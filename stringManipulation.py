af6269d2bd7f4e0dbe333f16212a2470bc42b6b5
VIKRAM <vikramria195@gmail.com>
2026-03-07 00:31:34 +0530

    String

--- stringManipulation.py
+++ stringManipulation.py
@@ -1,12 +1,29 @@
-import sys
+name="VikRam"
 
-if len(sys.argv) == 2:
-    print("Please enter your full name and last name as command line arguments.")
-    sys.exit()
+print(name.upper())
+print(name.lower())
 
-fullName = " ".join(sys.argv[1:])
+title="pYthon ProGRamMing"
+print(title.title())
 
-email=fullName.lower().replace(" ",".")+"@google.com"
+mobile="1234567890"
+print(mobile[:2]+"******"+mobile[-2:])
 
-print(fullName)
-print(email)
+location="Bangalore"
+replacedArea= location.replace("Bangalore","Mysore")
+print(replacedArea)
+
+uber_id="Hi, This is your Uber Id is: UB12342.Please does share others."
+
+id=uber_id.split(":")[1].split(".")[0].strip()
+print(id)
+
+zomota = "zomota is provide the coupen code for Zomota100"
+
+if "Zomota100" in zomota:
+    print("Zomota100 is available in the coupon.")
+
+LovName="vikram swe"
+
+initial="".join([name[0].upper() for name in LovName.split()])
+print(initial)
